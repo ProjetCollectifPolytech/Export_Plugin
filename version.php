e07e668e28a48037b4504cc63dfc0d619546da93
RogLag <hugo.rog@icloud.com>
2026-03-28 15:31:51 +0100

    fix: Update required Moodle version to 4.5+ in version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_gradefiller';
 $plugin->version = 2026032502;  // YYYYMMDDXX.
-$plugin->requires = 2023100900; // Moodle 4.3+.
+$plugin->requires = 2024100700; // Moodle 4.5+.
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v1.2.0-alpha';
